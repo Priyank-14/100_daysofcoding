d2a18953173ef20cdb76833ce67f24a7faa79f93
Priyank Sinha <priyanksinha2006@gmail.com>
2025-09-16 11:37:26 +0530

    Add files via upload

---
+++ pattern5.c
@@ -0,0 +1,33 @@
+/*Q50: Write a program to print the following pattern:
+*****
+  ****
+    ***
+      **
+        *
+
+Sample Test Cases:
+Input 1:
+Output 1:
+*****
+  ****
+    ***
+      **
+        *
+Input 2:
+Output 2:
+Note: Spaces indicate indentation.
+*/
+#include<stdio.h>
+int main(){
+    int i,j,space;
+    for(i=1;i<=5;i++){
+        for(space=1;space<i;space++){
+            printf(" ");
+        }
+        for(j=i;j<=5;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
